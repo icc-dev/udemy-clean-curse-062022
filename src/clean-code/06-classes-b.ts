6daa839e3fa05fd6212d7c5919445210e114a319
Ian Cardenas <icardenac@bancoripley.com>
2022-06-18 20:10:15 -0400

    init class 27

---
+++ src/clean-code/06-classes-b.ts
@@ -0,0 +1,63 @@
+(() => {
+  // without single responsability
+  type Gender = 'M' | 'F';
+
+  interface IPerson {
+    name: string;
+    gender: Gender;
+    birthdate: Date;
+  }
+
+  class Person {
+    constructor(
+      public name: string,
+      public gender: Gender,
+      public birthdate: Date
+    ) {}
+  }
+
+  class User extends Person {
+    public lastAccess: Date;
+
+    constructor(
+      public email: string,
+      public role: string,
+      name: string,
+      gender: Gender,
+      birthdate: Date
+    ) {
+      super(name, gender, birthdate);
+      this.lastAccess = new Date();
+    }
+
+    checkCredentials() {
+      return true;
+    }
+  }
+
+  /* class UserSettings extends User {
+    constructor(
+      public workingDirectory: string,
+      public lastOpenFolder: string,
+      email: string,
+      role: string,
+      name: string,
+      gender: Gender,
+      birthdate: Date
+    ) {
+      super(email, role, name, gender, birthdate);
+    }
+  }
+
+  const userSettings = new UserSettings(
+    '/usr/home',
+    '/home',
+    'iancardenas96@gmail.com',
+    'Admin',
+    'Ian',
+    'M',
+    new Date('1996-01-22')
+  );
+
+  console.log({ userSettings }); */
+})();
