3778af60be2a41ab4f463b85fc8f3d2b9b01d7dd
Ian Cardenas <icardenac@bancoripley.com>
2022-08-06 14:03:22 -0400

    finish class

--- src/solid/01-srp.ts
+++ src/solid/01-srp.ts
@@ -1,49 +1,69 @@
 (() => {
 
+  // product.interface.ts
   interface Product { 
       id:   number;
       name: string;
+  }
+
+  class ProductService {
+    getProduct(id: number) {
+      console.log(`Producto ${id}`, { id, name: 'Mac'});
+    }
+
+    addProduct(product: Product) {
+      console.log(`Guardando el producto`, product);
+    }
+  }
+
+  class NotificationService {
+    sendEmail(emails: string[], template: string, payload: any) {
+      console.log(`Enviando correo a los clientes`, { payload, template, emails });
+    }
   }
   
   // Usualmente, esto es una clase para controlar la vista que es desplegada al usuario
   // Recuerden que podemos tener muchas vistas que realicen este mismo trabajo.
   class ProductBloc {
   
-      loadProduct( id: number ) {
-          // Realiza un proceso para obtener el producto y retornarlo
-          console.log('Producto: ',{ id, name: 'OLED Tv' });
-      }
-  
-      saveProduct( product: Product ) {
-          // Realiza una petición para salvar en base de datos 
-          console.log('Guardando en base de datos', product );
-      }
-  
-      notifyClients() {
-          console.log('Enviando correo a los clientes');
-      }
-  
-      onAddToCart( productId: number ) {
-          // Agregar al carrito de compras
-          console.log('Agregando al carrito ', productId );
-      }
-  
+    constructor(public productService: ProductService, public notificationService: NotificationService) {
+
+    }
+
+    loadProduct(id: number) {
+      this.productService.getProduct(id);
+    }
+
+    saveProduct( product: Product ){
+      this.productService.addProduct(product);
+    }
+
+    notifyClients() {
+      this.notificationService.sendEmail(
+        ['iancardenas96@gmail.com'],
+        'to-user',
+        {}
+      )
+    }
+      
+  }
+
+  class CartBloc {
+    addToCart( productId: number) {
+      console.log(`Agregando al carrito ${productId}`);
+    }
   }
   
 
-
-  const productBloc = new ProductBloc();
+  // product.controller.ts
+  const productService = new ProductService();
+  const notificationService = new NotificationService();
+  const productBloc = new ProductBloc(productService, notificationService);
+  const cartBloc = new CartBloc();
 
   productBloc.loadProduct(10);
-  productBloc.saveProduct({ id: 10, name: 'OLED TV' });
+  productBloc.saveProduct({ id: 10, name: 'Mac' });
   productBloc.notifyClients();
-  productBloc.onAddToCart(10);
-
-
-
-
-
-
-
+  cartBloc.addToCart(10);
 
 })();
